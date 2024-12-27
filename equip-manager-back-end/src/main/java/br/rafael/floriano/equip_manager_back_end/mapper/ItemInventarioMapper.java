052a3a10209a061103fc946de855214a841f98a4
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 18:10:30 -0300

    feat: created new method to convert page of entity to page of 'ItemVisualizacaoDto'

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/mapper/ItemInventarioMapper.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/mapper/ItemInventarioMapper.java
@@ -2,7 +2,11 @@ package br.rafael.floriano.equip_manager_back_end.mapper;
 
 import br.rafael.floriano.equip_manager_back_end.dto.ItemVisualizacaoDto;
 import br.rafael.floriano.equip_manager_back_end.entity.InventoryItemEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ItemInventarioMapper {
@@ -16,6 +20,13 @@ public class ItemInventarioMapper {
                 inventoryItemEntity.getDescricao(),
                 inventoryItemEntity.getLocalizacao()
         );
+    }
+
+    public Page<ItemVisualizacaoDto> toDtoPagination(Page<InventoryItemEntity> inventoryItemEntityPage) {
+        final List<ItemVisualizacaoDto> dtoList = inventoryItemEntityPage.getContent().stream()
+                .map(this::toDto)
+                .toList();
+        return new PageImpl<>(dtoList, inventoryItemEntityPage.getPageable(), inventoryItemEntityPage.getTotalElements());
     }
 
 }
