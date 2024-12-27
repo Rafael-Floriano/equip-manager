a4c403a254161265bedb7fff34fdf237a27d1cb9
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 13:57:43 -0300

    fix: renamed attributes of 'InventoryItemEntity' class to Portuguese

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/entity/InventoryItemEntity.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/entity/InventoryItemEntity.java
@@ -1,17 +1,37 @@
 package br.rafael.floriano.equip_manager_back_end.entity;
 
+import br.rafael.floriano.equip_manager_back_end.enums.Disponibilidade;
+import br.rafael.floriano.equip_manager_back_end.enums.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Table(name = "inventory_item")
 public class InventoryItemEntity extends BaseEntity {
 
-    private String serialNumber;
-    private String availability;
-    private boolean status;
-    private String description;
-    private String localization;
-    private LocalDate movementDate;
+    @Column(name = "numero_de_serie", nullable = false, length = 5)
+    private String numeroDeSerie;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Disponibilidade disponibilidade;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false, length = 100)
+    private String descricao;
+
+    @Column(nullable = false, length = 250)
+    private String localizacao;
+
+    @Column(name = "data_movimentacao", nullable = false)
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime dataMovimentacao;
 
 }
