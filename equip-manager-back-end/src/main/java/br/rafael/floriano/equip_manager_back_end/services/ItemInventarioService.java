2572c84c46fb2f22e93f7d980d351e2d1edcbd49
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 16:01:27 -0300

    refactor: simplify 'ItemInventarioService' by adding field validation classes

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/services/ItemInventarioService.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/services/ItemInventarioService.java
@@ -1,20 +1,51 @@
 package br.rafael.floriano.equip_manager_back_end.services;
 
 import br.rafael.floriano.equip_manager_back_end.dto.ItemInventarioDto;
-import br.rafael.floriano.equip_manager_back_end.enums.MensagensDeErro;
-import com.google.common.base.Preconditions;
+import br.rafael.floriano.equip_manager_back_end.dto.ItemVisualizacaoDto;
+import br.rafael.floriano.equip_manager_back_end.entity.InventoryItemEntity;
+import br.rafael.floriano.equip_manager_back_end.mapper.ItemInventarioMapper;
+import br.rafael.floriano.equip_manager_back_end.repository.ItemInventarioRepository;
+import br.rafael.floriano.equip_manager_back_end.services.validadores.CodigoItemValidadorService;
+import br.rafael.floriano.equip_manager_back_end.services.validadores.DescricaoItemValidadorService;
+import br.rafael.floriano.equip_manager_back_end.services.validadores.LocalizacaoItemValidadorService;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemInventarioService {
 
-    public Object criarItemInventario(ItemInventarioDto itemInventarioDto) {
-        Preconditions.checkNotNull(itemInventarioDto.codigoItem(), MensagensDeErro.CODIGO_ITEM_NULO.getMessage());
-        Preconditions.checkNotNull(itemInventarioDto.descricao(), MensagensDeErro.DESCRICAO_ITEM_NULA.getMessage());
-        Preconditions.checkNotNull(itemInventarioDto.numeroDeSerie(), MensagensDeErro.NUMERO_SERIE_NULO.getMessage());
-        Preconditions.checkNotNull(itemInventarioDto.localizacao(), MensagensDeErro.LOCALIZACAO_NULA.getMessage());
-        return null;
+    @Autowired
+    private CodigoItemValidadorService codigoItemValidadorService;
+    @Autowired
+    private DescricaoItemValidadorService descricaoItemValidadorService;
+    @Autowired
+    private LocalizacaoItemValidadorService localizacaoItemValidadorService;
+    @Autowired
+    private ItemInventarioRepository itemInventarioRepository;
+    @Autowired
+    private ItemInventarioMapper itemInventarioMapper;
+
+    public ItemVisualizacaoDto criarItemInventario(ItemInventarioDto itemInventarioDto) {
+        defaultValidations(itemInventarioDto);
+        return itemInventarioMapper.toDto(
+                itemInventarioRepository.save(
+                        new InventoryItemEntity(
+                                itemInventarioDto.codigoItem(),
+                                itemInventarioDto.numeroDeSerie(),
+                                itemInventarioDto.descricao(),
+                                itemInventarioDto.localizacao()
+                        )
+                )
+        );
     }
+
+    public void defaultValidations(final ItemInventarioDto itemInventarioDto) {
+        codigoItemValidadorService.codigoItemIsValid(itemInventarioDto.codigoItem());
+        descricaoItemValidadorService.descricaoItemIsValid(itemInventarioDto.descricao());
+        localizacaoItemValidadorService.localizacaoItemIsValid(itemInventarioDto.localizacao());
+    }
+
 
 
 }
