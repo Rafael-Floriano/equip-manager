e6178d42620ad0854e03011ee28516d6c972d60f
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-26 23:11:10 -0300

    created equip-manager

---
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/EquipManagerBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.rafael.floriano.equip_manager_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipManagerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipManagerBackEndApplication.class, args);
+	}
+
+}
---
+++ equip-manager-back-end/src/test/java/br/rafael/floriano/equip_manager_back_end/EquipManagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rafael.floriano.equip_manager_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipManagerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
