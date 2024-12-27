587ee109b683488151fcb975e4c2f22a9fe10e4e
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 15:05:10 -0300

    feat: add more error messages to enum (all related to item code formatting errors)

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/enums/MensagensDeErro.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/enums/MensagensDeErro.java
@@ -5,7 +5,10 @@ public enum MensagensDeErro {
     CODIGO_ITEM_NULO("O código do item não pode ser nulo"),
     DESCRICAO_ITEM_NULA("A descrição do item não pode ser nula"),
     NUMERO_SERIE_NULO("O número de série não pode ser nulo"),
-    LOCALIZACAO_NULA("A localização não pode ser nula");
+    LOCALIZACAO_NULA("A localização não pode ser nula"),
+    CODIGO_ITEM_INVALIDO_DEVE_SER_ALFANUMERICO("O código do item deve ser alfanumérico"),
+    CODIGO_ITEM_INVALIDO_DEVE_TER_SETE_CARACTERS("O código do item deve ter no máximo 7 caracteres"),
+    CODIGO_ITEM_INVALIDO_NAO_PODE_SER_SEQUENCIAL("O código do item não pode ser sequencial");
 
     private final String message;
 
