d0879418276fdc46c1755d2afcf2526ae536d732
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 18:10:05 -0300

    refactored: removed 'dataMovimentacao' at record 'ItemInventarioDto'

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemInventarioDto.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemInventarioDto.java
@@ -1,10 +1,9 @@
 package br.rafael.floriano.equip_manager_back_end.dto;
 
-import java.time.LocalDateTime;
-
 public record ItemInventarioDto(
         String codigoItem,
-        String numeroDeSerie, String descricao,
-        String localizacao, LocalDateTime dataMovimentacao
+        String numeroDeSerie,
+        String descricao,
+        String localizacao
 ) {
 }
