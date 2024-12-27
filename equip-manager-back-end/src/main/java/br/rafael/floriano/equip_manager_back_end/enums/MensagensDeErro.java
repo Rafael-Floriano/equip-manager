9550b5e05201e20089d3fc48022e40e1312fd4ea
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 15:56:23 -0300

    feat: add new error messages to 'MensagensDeErro' enum

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/enums/MensagensDeErro.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/enums/MensagensDeErro.java
@@ -4,8 +4,11 @@ public enum MensagensDeErro {
 
     CODIGO_ITEM_NULO("O código do item não pode ser nulo"),
     DESCRICAO_ITEM_NULA("A descrição do item não pode ser nula"),
+    DESCRICAO_ITEM_TAMANHO_INVALIDO("A descrição do item não pode ter mais de 100 caracteres"),
     NUMERO_SERIE_NULO("O número de série não pode ser nulo"),
     LOCALIZACAO_NULA("A localização não pode ser nula"),
+    LOCALIZACAO_ITEM_TAMANHO_MINIMO("A localização não pode conter menos que 3 caracteres"),
+    LOCALIZACAO_ITEM_TAMANHO_MAXIMO("A localização não pode conter mais que 250 caracteres"),
     CODIGO_ITEM_INVALIDO_DEVE_SER_ALFANUMERICO("O código do item deve ser alfanumérico"),
     CODIGO_ITEM_INVALIDO_DEVE_TER_SETE_CARACTERS("O código do item deve ter no máximo 7 caracteres"),
     CODIGO_ITEM_INVALIDO_NAO_PODE_SER_SEQUENCIAL("O código do item não pode ser sequencial");
