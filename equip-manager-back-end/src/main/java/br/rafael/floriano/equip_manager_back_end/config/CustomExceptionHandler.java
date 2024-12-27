8094db05ed09a554e9057f916350a243ff47a7de
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-27 18:07:49 -0300

    feat: implement basic 'CustomExceptionHandler' class

---
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/config/CustomExceptionHandler.java
@@ -0,0 +1,18 @@
+package br.rafael.floriano.equip_manager_back_end.config;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Hidden
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> exceptionHandler(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+}
