bfec4ffdb7f13346651a3265e2331d945b26624c
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-29 20:20:28 -0300

    feat: created input for item search

---
+++ equip-manager-front-end/src/components/molecules/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Pesquise pelo item..."
+        v-model="searchValue"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      modelValue: {
+        type: String,
+        required: true,
+      },
+    },
+    computed: {
+      searchValue: {
+        get() {
+          return this.modelValue;
+        },
+        set(value) {
+          this.$emit('update:modelValue', value);
+        },
+      },
+    },
+  };
+  </script>
+  
