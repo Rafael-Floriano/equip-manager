d0a0f237380250ba45c1763e6e6b35cf41b61705
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-29 20:24:11 -0300

    feat: added router links in the app

--- equip-manager-front-end/src/App.vue
+++ equip-manager-front-end/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/search">Search</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
