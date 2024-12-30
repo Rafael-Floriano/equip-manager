fe6021e326d99a0d610e1d519b8a00fbdcf38155
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 18:22:58 -0300

    refactor: remove 'serialNumber' field from 'ItemInventoryDto'

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemInventarioDto.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemInventarioDto.java
@@ -2,7 +2,6 @@ package br.rafael.floriano.equip_manager_back_end.dto;
 
 public record ItemInventarioDto(
         String codigoItem,
-        String numeroDeSerie,
         String descricao,
         String localizacao
 ) {
