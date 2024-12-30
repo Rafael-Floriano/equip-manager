feaf433a9db1422b26425c24ee4740ef482d076d
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 14:29:03 -0300

    feat: implement new request to save items (used in item registration form)

--- equip-manager-front-end/src/services/inventoryService.ts
+++ equip-manager-front-end/src/services/inventoryService.ts
@@ -1,5 +1,6 @@
 import axiosClient from "@/config/axiosClient";
-import { InventoryResponse } from "@/types/pagination.types";
+import { ItemInventarioDto } from "@/types/item.types";
+import { InventoryItem, InventoryResponse } from "@/types/pagination.types";
 
 export const fetchInventoryItems = async (descricao:string, pagina:number): Promise<InventoryResponse> => {
     try {
@@ -15,3 +16,18 @@ export const fetchInventoryItems = async (descricao:string, pagina:number): Prom
       throw error;
     }
 };
+
+export const salvarNovoItem = async(itemInventarioDto:ItemInventarioDto): Promise<any> => {
+  console.log(itemInventarioDto);
+  try {
+    const response = await axiosClient.post<InventoryItem>('/item', itemInventarioDto);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export default interface ResponseError {
+  mensage:string
+}
