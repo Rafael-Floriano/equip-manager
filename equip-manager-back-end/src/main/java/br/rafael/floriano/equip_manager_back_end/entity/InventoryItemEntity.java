34a560a3f04f8152984a121fda09093e465eb126
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 19:38:03 -0300

    feat: implement SQL restriction to exclude items marked as deleted using soft delete

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/entity/InventoryItemEntity.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/entity/InventoryItemEntity.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "inventory_item")
+@SQLRestriction("excluido = false")
 public class InventoryItemEntity {
 
     @Column(name = "codigo_item", unique = true, length = 7)
