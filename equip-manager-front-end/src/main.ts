f26d3bfb5a4dbadcb552e9b3e080121683a10289
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-29 21:57:25 -0300

    Build: add Font Awesome dependencies to Vue app

--- equip-manager-front-end/src/main.ts
+++ equip-manager-front-end/src/main.ts
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTrash,faPen } from '@fortawesome/free-solid-svg-icons';
 import 'jquery';
 import router from './router/index';
-createApp(App).use(router).mount('#app');
+
+library.add(faTrash);
+library.add(faPen);
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router).mount('#app')
