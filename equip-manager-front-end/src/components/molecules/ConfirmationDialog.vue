1852d2aa5caae4f9018563212af5521c54a532b1
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-29 22:01:32 -0300

    feat: added confirmation dialog component for user actions

---
+++ equip-manager-front-end/src/components/molecules/ConfirmationDialog.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="confirmation-dialog" v-if="isVisible">
+      <div class="dialog-content">
+        <p class="user-select-auto" v-html="message"></p>
+        <button class="btn btn-primary" @click="confirm">Confirmar</button>
+        <button class="btn btn-secondary" @click="cancel">Cancelar</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      message: String,
+      isVisible: Boolean,
+    },
+    methods: {
+      confirm() {
+        this.$emit('confirm');
+      },
+      cancel() {
+        this.$emit('cancel');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .confirmation-dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+  }
+  
+  .dialog-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    text-align: center;
+  }
+  
+  button {
+    margin: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  </style>
+  
