4abb2d11d59c5d8efcda3a3271c1124ca7b1e5f5
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 14:27:54 -0300

    feat: create error alert message for backend request failures

---
+++ equip-manager-front-end/src/components/molecules/AlertMessage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div v-if="visible" class="confirmation-dialog alert alert-danger alert-dismissible fade show" role="alert">
+      <div class="dialog-content">
+        <strong>Oops!</strong> 
+      <p class="user-select-auto">{{ mensagem }}</p>
+      <button type="button" class="btn btn-warning" @click="closeAlert" aria-label="Close">Fechar</button>
+      </div>
+    </div>
+</template>
+  
+<script>
+  export default {
+    props: {
+      mensagem: String,
+    },
+    data() {
+      return {
+        visible: true,
+      };
+    },
+    methods: {
+      closeAlert() {
+        this.visible = false;
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+
+<style>
+  .confirmation-dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+  }
+  
+  .dialog-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    text-align: center;
+  }
+  
+  button {
+    margin: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+</style>
