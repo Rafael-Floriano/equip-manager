c61b254a9ea0523e1be4cd5c48bed886be782eee
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 19:34:52 -0300

    feat: implement button to return to the top of the list

---
+++ equip-manager-front-end/src/components/atoms/ScrollToTopButton.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <button v-if="showButton" class="scroll-to-top" @click="scrollToTop">
+        <font-awesome-icon icon="arrow-up" />
+      </button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    data() {
+      return {
+        showButton: false,
+      };
+    },
+    methods: {
+      scrollToTop() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      },
+      handleScroll() {
+        this.showButton = window.scrollY > 200;
+      },
+    },
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .scroll-to-top {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    padding: 18px 25px;
+    border-radius: 50%;
+    font-size: 24px;
+    cursor: pointer;
+    z-index: 1000;
+  }
+  
+  .scroll-to-top:hover {
+    background-color: #0056b3;
+  }
+  </style>
+  
