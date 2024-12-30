ee6256447c81261094fa0a970849a76e5846a96d
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-30 19:35:58 -0300

    feat: implement new Font Awesome icon (up arrow) for project UI

--- equip-manager-front-end/src/main.ts
+++ equip-manager-front-end/src/main.ts
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faTrash,faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrash,faPen, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import 'jquery';
 import router from './router/index';
 
 library.add(faTrash);
 library.add(faPen);
+library.add(faArrowUp);
 
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
