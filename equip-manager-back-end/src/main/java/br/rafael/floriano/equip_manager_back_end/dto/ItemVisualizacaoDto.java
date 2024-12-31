7cc320dc884d6721b41209a5177b2b18dba72414
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-31 19:29:46 -0300

    fix: adjust data types in the view DTO

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemVisualizacaoDto.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemVisualizacaoDto.java
@@ -1,13 +1,10 @@
 package br.rafael.floriano.equip_manager_back_end.dto;
 
-import br.rafael.floriano.equip_manager_back_end.enums.Disponibilidade;
-import br.rafael.floriano.equip_manager_back_end.enums.Status;
-
 public record ItemVisualizacaoDto(
         String codigoItem,
         String numeroDeSerie,
-        Disponibilidade disponibilidade,
-        Status status,
+        char disponibilidade,
+        char status,
         String descricao,
         String localizacao
 ) {
