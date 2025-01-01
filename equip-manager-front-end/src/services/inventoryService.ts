4c040bcefd176cbeab1375a976abfcaa704c9e5a
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-01-01 06:58:26 -0300

    refactored: removed unnecessary logs in inventory service

--- equip-manager-front-end/src/services/inventoryService.ts
+++ equip-manager-front-end/src/services/inventoryService.ts
@@ -2,60 +2,64 @@ import axiosClient from "@/config/axiosClient";
 import { ItemInventarioDto } from "@/types/item.types";
 import { InventoryItem, InventoryResponse } from "@/types/pagination.types";
 
-export const fetchInventoryItems = async (descricao:string, pagina:number): Promise<InventoryResponse> => {
-    try {
-        const response = await axiosClient.get<InventoryResponse>('/item', {
-            params: {
-              descricao,
-              pagina,
-            },
-          });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
+export const fetchInventoryItems = async (
+  descricao: string,
+  pagina: number
+): Promise<InventoryResponse> => {
+  const response = await axiosClient.get<InventoryResponse>("/item", {
+    params: {
+      descricao,
+      pagina,
+    },
+  });
+  return response.data;
 };
 
-export const salvarNovoItem = async(itemInventarioDto:ItemInventarioDto): Promise<any> => {
-  try {
-    const response = await axiosClient.post<InventoryItem>('/item', itemInventarioDto);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+export const salvarNovoItem = async (
+  itemInventarioDto: ItemInventarioDto
+): Promise<any> => {
+  console.log(itemInventarioDto);
+  const response = await axiosClient.post<InventoryItem>(
+    "/item",
+    itemInventarioDto
+  );
+  return response.data;
 };
 
 export default interface ResponseError {
-  mensage:string
+  mensage: string;
 }
 
-export const deleteItemPeloNumeroDeSerie = async(numeroDeSerie:String|undefined): Promise<any> => {
-  try {
-    const response = await axiosClient.delete<InventoryItem>(`/item/${numeroDeSerie}`);
-    return response.data;
-  } catch(error) {
-    throw error;
-  }
+export const deleteItemPeloNumeroDeSerie = async (
+  numeroDeSerie: String | undefined
+): Promise<any> => {
+  const response = await axiosClient.delete<InventoryItem>(
+    `/item/${numeroDeSerie}`
+  );
+  return response.data;
 };
 
-export const fetchItemByNumeroDeSerie = async (numeroDeSerie:String|undefined): Promise<InventoryItem> => {
-  try {
-    const response = await axiosClient.get<InventoryItem>(`/item/${numeroDeSerie}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+export const fetchItemByNumeroDeSerie = async (
+  numeroDeSerie: String | undefined
+): Promise<InventoryItem> => {
+  const response = await axiosClient.get<InventoryItem>(
+    `/item/${numeroDeSerie}`
+  );
+  return response.data;
 };
 
-export const putItemByNumeroDeSerie = async (numeroDeSerie:String|undefined, itemInventarioDto:InventoryItem | null): Promise<InventoryItem> => {
-  try {
-    const response = await axiosClient.put<InventoryItem>('/item', itemInventarioDto, {
+export const putItemByNumeroDeSerie = async (
+  numeroDeSerie: String | undefined,
+  itemInventarioDto: InventoryItem | null
+): Promise<InventoryItem> => {
+  const response = await axiosClient.put<InventoryItem>(
+    "/item",
+    itemInventarioDto,
+    {
       params: {
-        numeroDeSerie
+        numeroDeSerie,
       },
-    });
-    return response.data;
-  } catch(error) {
-    throw error;
-  }
-}
+    }
+  );
+  return response.data;
+};
