e4672949c5d8b42bdd925a123720d225b672953a
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-01-01 15:21:50 -0300

    feat: create global method to format dates

---
+++ equip-manager-front-end/src/utils/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (data: string): string => {
+    const date = new Date(data);
+    const dia = date.getDate().toString().padStart(2, '0');
+    const mes = (date.getMonth() + 1).toString().padStart(2, '0');
+    const ano = date.getFullYear();
+    const horas = date.getHours().toString().padStart(2, '0');
+    const minutos = date.getMinutes().toString().padStart(2, '0');
+
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}`;
+};
