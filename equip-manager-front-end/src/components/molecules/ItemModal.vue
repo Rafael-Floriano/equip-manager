bb18c4b60976231b6763c3f3340ef57cbc57eb31
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-01-01 15:22:30 -0300

    feat: add movement date to the item modal

--- equip-manager-front-end/src/components/molecules/ItemModal.vue
+++ equip-manager-front-end/src/components/molecules/ItemModal.vue
@@ -1,48 +1,26 @@
 <template>
   <div class="modal-overlay" @click.self="close">
     <div class="modal-content">
-      <h2>{{ isEditMode() ? "✏️ Editar Item" : "✏️ Cadastrar Item"}}</h2>
+      <h2>{{ isEditMode() ? "✏️ Editar Item" : "✏️ Cadastrar Item" }}</h2>
       <div class="d-flex justify-content-center align-items-center mt-3">
         <div v-if="loading" class="spinner-border text-primary" role="status"></div>
       </div>
       <form @submit.prevent="saveChanges">
         <div class="form-group">
           <label for="codigoItem">🔢 Código:</label>
-          <input 
-            type="text" 
-            id="codigoItem" 
-            v-model="item.codigoItem" 
-            class="form-control" 
-            :readonly="isEditMode()" 
-          />
+          <input type="text" id="codigoItem" v-model="item.codigoItem" class="form-control" :readonly="isEditMode()" />
         </div>
         <div class="form-group" v-if="isEditMode()">
           <label for="numeroDeSerie">🔧 Número de Série:</label>
-          <input 
-            type="text" 
-            id="numeroDeSerie" 
-            v-model="item.numeroDeSerie" 
-            class="form-control"
-            readonly
-          />
+          <input type="text" id="numeroDeSerie" v-model="item.numeroDeSerie" class="form-control" readonly />
         </div>
         <div class="form-group">
           <label for="descricao">📝 Descrição:</label>
-          <input 
-            type="text" 
-            id="descricao" 
-            v-model="item.descricao" 
-            class="form-control"
-          />
+          <input type="text" id="descricao" v-model="item.descricao" class="form-control" />
         </div>
         <div class="form-group">
           <label for="localizacao">📍 Localização:</label>
-          <input 
-            type="text" 
-            id="localizacao" 
-            v-model="item.localizacao" 
-            class="form-control"
-          />
+          <input type="text" id="localizacao" v-model="item.localizacao" class="form-control" />
         </div>
         <div class="form-group" v-if="isEditMode()">
           <label for="disponibilidade">📦 Disponibilidade:</label>
@@ -58,6 +36,11 @@
             <option :value="'I'">Inativo ❌</option>
           </select>
         </div>
+        <div class="form-group" v-if="isEditMode()">
+          <label for="status">📅 Data de movimentação:</label>
+          <input type="text" id="dataMovimentacao" :value="formatarData(item.dataMovimentacao)" class="form-control"
+            readonly />
+        </div>
         <div class="form-group">
           <button type="submit" class="btn btn-success">Salvar</button>
           <button type="button" class="btn btn-secondary" @click="close">Cancelar</button>
@@ -66,7 +49,7 @@
     </div>
   </div>
 
-  <AlertMessage v-if="showAlertModal" :mensagem="mensagemDeErro"/>
+  <AlertMessage v-if="showAlertModal" :mensagem="mensagemDeErro" />
 </template>
 
 <script lang="ts">
@@ -76,6 +59,7 @@ import { fetchItemByNumeroDeSerie, putItemByNumeroDeSerie, salvarNovoItem } from
 import { AcoesModal } from '@/constants/enums/AcoesModal';
 import AlertMessage from './AlertMessage.vue';
 import { AxiosError } from 'axios';
+import { formatDate } from '@/utils/formatDate';
 
 export default defineComponent({
   components: {
@@ -94,28 +78,31 @@ export default defineComponent({
   data() {
     return {
       item: {
-      codigoItem: '',
-      numeroDeSerie: '',
-      descricao: '',
-      localizacao: '',
-      disponibilidade: '',
-      status: ''
-    } as InventoryItem,
-    loading: false as boolean,
-    mensagemDeErro: '' as string,
-    showAlertModal: false as boolean,
+        codigoItem: '',
+        numeroDeSerie: '',
+        descricao: '',
+        localizacao: '',
+        disponibilidade: '',
+        status: ''
+      } as InventoryItem,
+      loading: false as boolean,
+      mensagemDeErro: '' as string,
+      showAlertModal: false as boolean,
     };
   },
   methods: {
+    formatarData(data: string) {
+      return formatDate(data);
+    },
     isEditMode(): boolean {
       return this.acaoModal === AcoesModal.editar;
     },
-    async saveChanges():Promise<any> {
+    async saveChanges(): Promise<any> {
       try {
-        let item:InventoryItem;
+        let item: InventoryItem;
         if (this.isEditMode()) {
           item = await putItemByNumeroDeSerie(
-            this.numeroDeSerie, 
+            this.numeroDeSerie,
             this.item
           );
         } else {
@@ -130,7 +117,7 @@ export default defineComponent({
         }
         this.$emit('save', item);
         this.close();
-      } catch(error) {
+      } catch (error) {
         const axiosError = error as AxiosError;
         if (axiosError.response && axiosError.response.data) {
           this.mensagemDeErro = axiosError.response.data;
@@ -148,7 +135,8 @@ export default defineComponent({
         descricao: '',
         localizacao: '',
         disponibilidade: '',
-        status: ''
+        status: '',
+        dataMovimentacao: '',
       };
       this.$emit('close');
     },
@@ -165,61 +153,60 @@ export default defineComponent({
   },
 });
 </script>
-  
+
 <style scoped>
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 1000;
-  }
-  
-  .modal-content {
-    background: white;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
-    width: 90%;
-    max-width: 500px;
-  }
-  
-  .modal-content h2 {
-    margin-top: 0;
-    font-size: 1.5rem;
-  }
-  
-  .form-group {
-    margin-bottom: 15px;
-  }
-  
-  .form-group label {
-    display: block;
-    font-size: 1rem;
-    margin-bottom: 5px;
-  }
-  
-  .form-group input,
-  .form-group select {
-    width: 100%;
-    padding: 8px;
-    font-size: 1rem;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-  }
-  
-  .form-group button {
-    margin-top: 10px;
-    padding: 10px 20px;
-  }
-  
-  .btn-secondary {
-    margin-left: 10px;
-  }
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+  width: 90%;
+  max-width: 500px;
+}
+
+.modal-content h2 {
+  margin-top: 0;
+  font-size: 1.5rem;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+.form-group label {
+  display: block;
+  font-size: 1rem;
+  margin-bottom: 5px;
+}
+
+.form-group input,
+.form-group select {
+  width: 100%;
+  padding: 8px;
+  font-size: 1rem;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+.form-group button {
+  margin-top: 10px;
+  padding: 10px 20px;
+}
+
+.btn-secondary {
+  margin-left: 10px;
+}
 </style>
-  
