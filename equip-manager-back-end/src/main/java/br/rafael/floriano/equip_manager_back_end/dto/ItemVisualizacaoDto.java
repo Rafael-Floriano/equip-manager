8d439c3faf9ac284dab389db3f8b793659201f46
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-01-01 15:20:24 -0300

    feat: add dataMovimentacao field to ItemVisualizacaoDto

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemVisualizacaoDto.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/dto/ItemVisualizacaoDto.java
@@ -1,11 +1,16 @@
 package br.rafael.floriano.equip_manager_back_end.dto;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
 public record ItemVisualizacaoDto(
         String codigoItem,
         String numeroDeSerie,
         char disponibilidade,
         char status,
         String descricao,
-        String localizacao
+        String localizacao,
+        LocalDateTime dataMovimentacao
 ) {
 }
