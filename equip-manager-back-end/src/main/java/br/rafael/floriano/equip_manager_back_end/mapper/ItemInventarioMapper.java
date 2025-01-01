de28369065729dd794eac84935a1425778df5561
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-01-01 16:20:41 -0300

    refactored: adjusted imports of converters (IN iTEM INVENTÁRIO MAPPER)

--- equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/mapper/ItemInventarioMapper.java
+++ equip-manager-back-end/src/main/java/br/rafael/floriano/equip_manager_back_end/mapper/ItemInventarioMapper.java
@@ -1,7 +1,6 @@
 package br.rafael.floriano.equip_manager_back_end.mapper;
 
 import br.rafael.floriano.equip_manager_back_end.dto.ItemVisualizacaoDto;
-import br.rafael.floriano.equip_manager_back_end.entity.DisponibilidadeConverter;
 import br.rafael.floriano.equip_manager_back_end.entity.InventoryItemEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
